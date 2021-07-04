1cffb081cf2f76346b9d6a8cd629e65bfb024e0f
HardID <hardi.hipstercoder@gmail.com>
2021-07-04 12:24:37 +0700

    dashboard
    dashboard

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,41 @@
 	  </div>
 	  <!-- /.card-header -->
 	  <div class="card-body">
-	   
+      <div class="row">
+        <div class="col-lg-6 col-6">
+              <!-- small box -->
+          <?php 
+          $sql = "SELECT * FROM tb_nasabah";
+          $query  = mysqli_query($conn,$sql);
+          ?>
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3><?php echo mysqli_num_rows($query) ?></h3>
+
+                  <p>Nasabah KRP</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+              </div>
+        </div>
+        <div class="col-lg-6 col-6">
+          <!-- small box -->
+          <?php 
+          $sql = "SELECT * FROM tb_user WHERE level != 0";
+          $query  = mysqli_query($conn,$sql);
+          ?>
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3><?php echo mysqli_num_rows($query) ?></h3>
+              <p>Admin</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+          </div>
+        </div>
+      </div>
 	  </div>
 	  <!-- /.card-body -->
 	</div>
